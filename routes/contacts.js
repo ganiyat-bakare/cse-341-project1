e07918c6a210fc3745bc5b49120f3c3d877e76dd
ganiyat-bakare <bak24006@byui.edu>
2025-09-14 01:36:08 +0100

    Adding Contacts Part 2

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,8 +5,12 @@ const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll); 
 
-// router.post('/', contactsController.createContact);
-
 router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const mongodb = require('./data/database');
 const app = express();
+const bodyParser = require('body-parser');
+
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+// app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 app.use('/', require('./routes'));
 
 mongodb.initDB((err) => {
